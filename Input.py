372f810af759d78c9a388679017ea5482be14b72
itsmansoor <mansoorkhaksar22@gmail.com>
2026-04-14 11:46:42 +0500

    added python concepts: math  and input

---
+++ Input.py
@@ -0,0 +1,20 @@
+name = input('enter your name')
+print(name)
+
+a = input("Enter number: ")
+b = input("Enter number: ")
+print(a + b) # return string 
+print(int(a) + int(b))
+
+
+age = int(input('enter your age'))
+print(age)
+
+price = float(input("Enter price: "))
+print(price)
+
+myname = input("Enter name: ")
+Myage = int(input("Enter age: "))
+
+print("Hello", myname)
+print(" your Next year age:", Myage + 1)
---
+++ Maths.py
@@ -0,0 +1,12 @@
+import math
+import math as m   # use m instead of using math
+
+ #from math import sqrt,pow || alternative way to import specific functions 
+
+print(math.sqrt(25))
+
+print(math.floor(3.9))
+print(math.ceil(3.1))
+
+
+print(math.pow(3,2))
